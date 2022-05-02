412f1c0249fd76aa4f120d22a9e992001ad70a16
Hassan Ndam <ndamfiverr]gmail.com>
2022-05-02 20:03:45 +0200

    retrait de la balise Main

--- footer.php
+++ footer.php
@@ -3,8 +3,8 @@
 <footer>
   <div class="row">
     <div class="col-6 col-md">
-      <a href="<?php echo get_site_url();?>" alt="Aller sur la page d'accueil">
-      <img src="<?php echo (get_template_directory_uri(). './assets/icone/FavIcone1.png');?>" class="img-fluid" alt="..." width="33%" height="10%">
+      <a href="<?php echo get_site_url();?>" alt="Aller sur la page d'accueil" title="Aller sur la page d'accueil">
+      <img src="<?php echo (get_template_directory_uri(). '/assets/icone/favicone.png');?>" class="img-fluid" alt="..." width="33%" height="10%">
       </a>
     </div>
     <div class="col-6 col-md">
@@ -44,16 +44,15 @@
       </a> 
     </div>
 </footer>
-</main> 
     
 <?php 
 wp_footer();
 ?>  
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script> 
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" defer integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" defer integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script> 
 
-<script src="<?php echo(get_template_directory_uri() . '/assets/js/main.js') ?>"></script>
+<script src="<?php echo(get_template_directory_uri() . '/assets/js/main.js') ?>" defer></script>
 
 </body>
 </html>
