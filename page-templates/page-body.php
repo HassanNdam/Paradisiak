f7f8ed01e11ed38d2aa7b4a4c629ff8276de7586
Hassan Ndam <ndamfiverr]gmail.com>
2022-05-03 10:09:18 +0200

    Retrait du fichier main.js pour la correction du bug sur le slide

--- page-templates/page-body.php
+++ page-templates/page-body.php
@@ -1,17 +1,15 @@
 
-  <!-- Fichiers d'entête à retirer et bien placer-->
-
 <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" rel="stylesheet" />
 <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" rel="stylesheet" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-<script src="<?php echo(get_template_directory_uri() . './assets/js/main.js') ?>"></script>
+<script src="<?php echo(get_template_directory_uri() . '/assets/js/main.js') ?>" defer></script>
 
-<div class="container marketing slideanim">
+<div class="container marketing ">
       <h1 class="creer-site-titre"> <span id="apropos"></span>
        <span class="paradisiak-text2"><span class="point-1">P</span>aradisiak,</span>
       </h1> 
-      <h1 class="creer-texte-ligne">
+      <h1 class="creer-texte-ligne slideanim">
         l<span class="point-2">'</span>agence Web des recruteurs<span class="bleu-texte"> !</span>
       </h1>
 </div>
@@ -22,7 +20,7 @@
                 recrutement ou une entreprise qui souhaite dynamiser sa communication RH ou sa politique de recrutement nous avons la solution !</p>
               </div>
               <div class="col-md-6 slideanim">
-                <img src="<?php echo (get_template_directory_uri(). './assets/img/Colonnetest.png');?>" class="img-fluid" alt="..." width="100%" height="100%">
+                <img src="<?php echo (get_template_directory_uri(). '/assets/img/paradisiak.png');?>" class="img-fluid" alt="..." width="100%" height="100%">
               </div>
             </div>
             <hr class="featurette-divider">
@@ -31,7 +29,7 @@
 <div class="container etape-3">
     <div class="row align-items-md-stretch espace-demarquez">
       <div class="col-md-6 slideanim">
-        <div class="h-100 p-5 text-dark bg-light border rounded-3 dark-bordure dark-bordure-vert">
+        <div class="h-100 p-5 text-dark bg-light border rounded-3">
              <div class="row">
                   <div class="col-6 col-sm-3"><h1 class="etape">1<span class="point-1">.</span></h1></div>
                   <div class="col"><h2 class="espace">Attirez les meilleurs talents</h2></div>
@@ -44,7 +42,7 @@
         </div>
       </div>
       <div class="col-md-6 slideanim">
-        <div class="h-100 p-5 text-white bg-dark rounded-3 dark-bordure dark-bordure-jaune">
+        <div class="h-100 p-5 text-white bg-dark rounded-3">
             <div class="row">
                   <div class="col-2 col-sm-3"><h1 class="etape">2<span class="point-3">.</span></h1></div>
                   <div class="col"><h2 class="espace">Transformez vos visiteurs en candidats</h2></div>
@@ -57,7 +55,7 @@
     </div>
     <div class="row align-items-md-stretch">
        <div class="col-md-6 slideanim">
-        <div class="h-100 p-5 bg-dark border rounded-3 dark-bordure dark-bordure-rose">
+        <div class="h-100 p-5 bg-dark border rounded-3">
             <div class="row">
                   <div class="col-2 col-sm-3"><h1 class="etape">3<span class="point-2">.</span></h1></div>
                   <div class="col"><h2 class="espace">Optimisez le référencement naturel</h2></div>     
@@ -65,17 +63,17 @@
             <p>Les solutions de Paradisiak sont pensées pour être très efficaces dans la création de l’afflux de candidats.
                 </p>
                 <br>
-             <h4 class="point-2">Boostez votre notoriété !</h4>        
+             <h4 class="point-2 espace-3-4">Boostez votre notoriété !</h4>
         </div>
       </div> 
       <div class="col-md-6 slideanim">
-        <div class="h-100 p-5 text-dark bg-light border  rounded-3 dark-bordure dark-bordure-orange">
+        <div class="h-100 p-5 text-dark bg-light border rounded-3">
             <div class="row">
                       <div class="col-2 col-sm-3"><h1 class="etape">4<span class="point-4">.</span></h1></div>
                       <div class="col"><h2 class="espace">Connectez-vous à votre logiciel de recrutement</h2></div>     
             </div>
             <p>Créez un poste, diffusez-le en 1 clic sur votre site carrière et retrouver tous vos candidats dans votre logiciel de recrutement.</p>
-             <h4 class="point-4"> Gagnez du temps !</h4> 
+             <h4 class="point-4 espace-3-4"> Gagnez du temps !</h4>
          </div>
       </div>
   </div>
@@ -87,33 +85,31 @@
     </a> 
 </div>
 
-
   <!-- Blocs exemples de site -->
  
 <div class="container back-exemple slideanim"><span id="exemples"></span>
   <hr class="featurette-divider">
         <h2 class="exemple1">Exemples de sites carrière</h2>
-        <p class="lead spealise1">Découvrez tous nos sites carrières conçus par <span class="texte-vert">Paradisiak!</span></p>  
+        <p class="lead spealise1">Découvrez quelques sites carrières conçus par  <span class="texte-vert">Paradisiak !</span></p>  
 </div>
-
 
 
 <div class="container-xxl">
     <div class="owl-carousel owl-theme">
             <div class="item first prev hovereffect">
-                      <img src="<?php echo(get_template_directory_uri() . '/assets/portfolio/Dmr_Home.png') ?>" class="img-fluid" alt="Compiene">
+                      <img src="<?php echo(get_template_directory_uri() . '/assets/portfolio/dmr_home.png') ?>" class="img-fluid" alt="Compiene">
                       <div class="overlay">
                           <a class="info" href="https://admr76.candidater.fr/" target="_blank">Voir la page carrière </a>
                       </div>
             </div>
             <div class="item show hovereffect">
-                      <img src="<?php echo(get_template_directory_uri() . '/assets/portfolio/Pichet_home.png') ?>" class="img-fluid" alt="Pichet">
+                      <img src="<?php echo(get_template_directory_uri() . '/assets/portfolio/pichet_home.png') ?>" class="img-fluid" alt="Pichet">
                       <div class="overlay">
                           <a class="info" href="https://emploi.pichet.fr/" target="_blank">Voir la page carrière</a>
                       </div>
             </div>
             <div class="item next hovereffect">
-                      <img src="<?php echo(get_template_directory_uri() . '/assets/portfolio/Intersport_Home.png') ?>" class="img-fluid" alt="Intersport" >
+                      <img src="<?php echo(get_template_directory_uri() . '/assets/portfolio/intersport_home.png') ?>" class="img-fluid" alt="Intersport" >
                       <div class="overlay">
                           <a class="info" href="https://recrutement.intersport.fr/" target="_blank">
                             Voir la page carrière
@@ -121,14 +117,13 @@
                       </div>
             </div>
             <div class="item last hovereffect">
-                      <img src="<?php echo(get_template_directory_uri() . '/assets/portfolio/Compiegne_Home.png') ?>" class="img-fluid" alt="Compiegne">
+                      <img src="<?php echo(get_template_directory_uri() . '/assets/portfolio/compiegne_home.png') ?>" class="img-fluid" alt="Compiegne">
                       <div class="overlay">
                                 <a class="info" href="https://recrutement.agglo-compiegne.fr/" target="_blank">Voir la page carrière</a>
                       </div>
             </div>
       </div>
 </div>
-
 
 
 <h2 class="texte-confiance">Ils nous font confiance !</h2>
@@ -181,15 +176,15 @@
             </div>
 
 
-  <!-- Dernier block accueil -->
+  <!--Dernier block accueil-->
 
 <div class="container-fluid text-center slideanim">
         <div class="row" >
-                  <h2 class="featurette-heading pensez"> <span class="point-1">C</span>onfiez votre site carrière à nos experts <span class="point-1">RH</span> !</h2> 
+                   <h2 class="featurette-heading pensez"> <span class="point-1">C</span>onfiez votre site carrière à nos experts <span class="point-1">RH</span> !</h2> 
                    <a class="devis-pensez" href="<?php echo $page_contact; ?>">
                       <button type="button" class="btn btn-success btn-lg text-white">Demandez un devis </button>
-                  </a>     
+                   </a>     
         </div>
 </div>
-<img src="<?php echo (get_template_directory_uri(). './assets/img/test.png');?>" class="img-fluid" alt="..." width="100%" height="10%">
+<img src="<?php echo (get_template_directory_uri(). '/assets/img/footer_image.png');?>" class="img-fluid" alt="..." width="100%" height="10%">
 
