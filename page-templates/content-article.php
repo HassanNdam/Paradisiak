7c870a56d77112bf085c61aebd6f3ff5e166caa9
Hassan Ndam <ndamfiverr]gmail.com>
2022-05-10 09:48:07 +0200

    ajout de content-article pour afficher la des description des articles

---
+++ page-templates/content-article.php
@@ -0,0 +1,59 @@
+
+<main class="container">
+<div class="text-start single-article">
+    <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="<?php echo get_site_url();?>">Accueil</a></li>
+        <li class="breadcrumb-item"><a href="<?php echo $page_blog; ?>">Notre blog</a></li>
+        <li class="breadcrumb-item active" aria-current="page"><?php echo the_title();?></li>
+    </ol>
+    </nav>
+    <h1 class="titre-article "><?php echo the_title();?></h1>
+</div>
+
+<div class="container">
+    <img class="rounded mx-auto d-block" src="<?php the_post_thumbnail_url('medium'); ?>" alt="image" width="600" height="300">
+</div>
+
+<div class="row g-5 espace-article">
+    <div class="col-md-8">
+    <article class="blog-post">
+    <p class="blog-post-meta"> Publié le <?php the_date();?> par <strong> <?php the_author();?></strong></p>
+        <hr>
+        
+      </article>
+
+            <?php
+
+            the_content();
+
+            ?>
+    </div>
+
+    <div class="col-md-4">
+      <div class="position-sticky" style="top: 2rem;">
+        <div class="p-4 mb-3 bg-light rounded box-pourquoi">
+          <h4 class="idee-site">Catégories</h4>
+            <ol class="list-unstyled mb-0 text-center">
+            <li><a class="categorie" href="#">Ici </a></li> 
+                     <!-- <?php the_category(); ?>
+                         <?php the_tags();?> -->
+            </ol>
+          <p class="mb-0"></p>
+        </div>
+            <div class="text-center"><br><br>
+                <a class="lien-contagct-blog" href="<?php echo $page_contact; ?>">
+                      <button type="button" class="btn btn-success btn-lg text-white">Demandez un devis </button>
+                </a>
+            </div>
+        <div class="p-4 box-pourquoi">
+          <h4 class="idee-site">Archives sur notre blog</h4>
+          <ol class="list-unstyled mb-0 text-center">
+            <li><a class="categorie" href="#">Ici</a></li>
+          </ol>
+        </div>  
+        </div>
+      </div>
+    </div>
+  </div>
+  </main>
