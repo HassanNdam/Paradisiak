6c09419cc5aed57264a6104905561e9eed3d9323
Hassan Ndam <ndamfiverr]gmail.com>
2022-05-10 09:49:07 +0200

    ajout fichier archive non utilisé pour linstant

---
+++ page-templates/content-archive.php
@@ -0,0 +1,42 @@
+<div class="container">
+
+  <!-- 
+      Code affichage blog deux colonnes à garder
+  
+  <div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary">Catégorie</strong>
+          <h3 class="mb-0">Titre</h3>
+          <div class="mb-1 text-muted">Date publication</div>
+          <p class="card-text mb-auto">Article.......</p>
+          <a href="#" class="stretched-link">Lire la suite</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-success">Catégorie</strong>
+          <h3 class="mb-0">Titre</h3>
+          <div class="mb-1 text-muted">Date publication</div>
+          <p class="mb-auto">Article.....</p>
+          <a href="#" class="stretched-link">Lire la suite...</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+        </div>
+      </div>
+    </div>
+  </div>  -->
+
+    <?php
+        the_excerpt();
+    ?>
+</div>
