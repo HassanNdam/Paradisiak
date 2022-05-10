19099f1e0d3c97987dde8638e3dd19c38ad4a630
Hassan Ndam <ndamfiverr]gmail.com>
2022-05-10 10:29:17 +0200

    ajout espace sur le bouton

--- page-templates/page-body.php
+++ page-templates/page-body.php
@@ -9,7 +9,7 @@
           <h1 class="site-carriere"> Votre site carrière <strong>sur-mesure</strong> </h1>
           <br>
           <p class="lead fw-normal">Attirez les meilleurs talents et faites exploser <br>votre référencement !</p>
-            <a href="<?php echo $page_contact; ?>"><button type="button" class="btn btn-success text-white hover-vert " >Demandez un devis</button>
+            <a href="<?php echo $page_contact; ?>"><button type="button" class="btn btn-success text-white espace-btn-header" >Demandez un devis</button>
           </a>
         </div>
 </div>
