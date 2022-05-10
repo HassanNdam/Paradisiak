bb09a868aaa4e115ab8c900bccbc6789f77fe284
Hassan Ndam <ndamfiverr]gmail.com>
2022-05-10 09:32:18 +0200

    Modification taille logo

--- header.php
+++ header.php
@@ -26,18 +26,18 @@
 <header class="site-header"> 
   <nav class="navbar navbar-default navbar-expand-md d-flex container flex-column flex-md-row justify-content-between">
       <a class="navbar-brand" href="<?php echo get_site_url();?>" aria-label="Product" >
-            <img src="<?php echo (get_template_directory_uri(). '/assets/logo/logo-large.png');?>"  alt="" height="85" width="265"> 
+            <img src="<?php echo (get_template_directory_uri(). '/assets/logo/logo-large.png');?>"  alt="" height="76" width="211"> 
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="true" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="nav-link d-md-inline-block apropos" href="#apropos">
+      <a class="d-md-inline-block apropos" href="#apropos">
       <span class="point-4">À</span> propos de nous
       </a>
-      <a class="nav-link d-md-inline-block exemples" href="#exemples">
+      <a class="d-md-inline-block exemples" href="#exemples">
       <span class="point-2">N</span>os réalisations
       </a>
-      <a class="nav-link d-md-inline-block blog" href="<?php echo $page_blog; ?>">
+      <a class="d-md-inline-block blog" href="<?php echo $page_blog; ?>">
       <span class="point-1">N</span>otre blog
       </a>
       <a class="d-md-inline-block contact" href="<?php echo $page_contact; ?>">
@@ -47,14 +47,5 @@
         <button class="btn btn-success text-white display-btn-top"><i class="fa fa-phone"></i> 01 45 26 08 73</button>
 	    </a>
   </nav>
-  <div class="position-relative overflow-hidden bg-dark ">
-        <div class="header-text container">
-          <h1 class="site-carriere"> Votre site carrière <strong>sur-mesure</strong> </h1>
-          <br>
-          <p class="lead fw-normal">Attirez les meilleurs talents et faites exploser <br>votre référencement !</p>
-            <a href="<?php echo $page_contact; ?>"><button type="button" class="btn btn-success text-white hover-vert " >Demandez un devis</button>
-          </a>
-        </div>
- </div>
 </header>
 
